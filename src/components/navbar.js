811fe15d1b9eabc9e58e7712f58c3ba60f263195
Hirendra-Gurnani <92821585+Hirendra-Gurnani@users.noreply.github.com>
2023-12-17 11:17:53 +0530

    Utilized Material UI styling and removed external css for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,25 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
-// import hematite from '../assets/imgs/logo/hemtatite.png'
-// import iexcel from '../assets/imgs/logo/iexcel.png'
-import "../assets/css/navbar.css";
+// import "../assets/css/navbar.css";
 
 function Navbar() {
   return (
     <>
-      <AppBar position="static" sx={{ height: 80, backgroundColor: "#ffffff" }}>
+      <AppBar
+        position="static"
+        sx={{
+          height: 80,
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+          fontSize: "25px",
+          color: "#237ba1",
+        }}
+      >
         <Container maxWidth="xl">
           <div className="App header">
             <h1 className="title">Employee Dashboard</h1>
